3fb59334333b4e4009bc6017f6ce3773a2706d43
AhmedElsharkawyCS <ahmedelsharkawy.fcih@gmail.com>
2021-02-15 13:39:10 +0200

    update container github actions

--- apps/container/src/App.js
+++ apps/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default function App() {
   return (
     <div>
-      <h1>hey container</h1>
+      <h1>hey container!</h1>
       <hr />
       <MarketingApp />
     </div>
--- apps/marketing/config/webpack.prod.js
+++ apps/marketing/config/webpack.prod.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
+
 const prodConfig = {
   mode: "production",
   output: {
