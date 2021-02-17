00b8406ca7fd17ca773d949ba5f43dced2f85fba
AhmedElsharkawyCS <ahmedelsharkawy.fcih@gmail.com>
2021-02-17 20:13:42 +0200

    fix class collision issue

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # Micro-Frontend-Application
 
-## `Main concepts/Feature`
+## live demo
+
+- [ECommerce Micro-Frontend Application](https://d27khfmjokixtz.cloudfront.net/)
+
+## `Main concepts/Feature/Topics`
 
 - isolate every single app
-- use webpack module federation and shared dependencies between multiple apps
+- use webpack module federation and shared dependencies between multiple applications
 - link multiple apps together
+- Create CI/CD pipeline and deploy to AWS
+- configure AWS `S3`,`CloudFront`
+- handling CSS in the whole micro-frontend (css class collision issue) when we use same css-in-js lib
 
 ## `Library & modules`
 
--- apps/container/src/App.js
+++ apps/container/src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { createGenerateClassName, StylesProvider } from "@material-ui/core/styles";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 
+//to fix class name collision issue
+const generateClassName = createGenerateClassName({ productionPrefix: "ca" });
 export default function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <MarketingApp />
+          </div>
+        </BrowserRouter>
+      </StylesProvider>
+    </div>
   );
 }
